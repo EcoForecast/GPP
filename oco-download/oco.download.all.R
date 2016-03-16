d167369067a70a6ca875c604c16a26b061dc2b53
Alexey Shiklomanov <ashiklom@bu.edu>
2016-03-16 18:06:51 -0500

    Add code to draw figure

--- oco-download/oco.download.all.R
+++ oco-download/oco.download.all.R
@@ -1,7 +1,5 @@
 # Download data up to latest
-
 source("oco-download/oco.download.R")
-try(file.remove("oco-download/fluorescence.csv"))
 start.date <- as.POSIXlt("2014-09-07", tz = "GMT")
 end.date <- as.POSIXlt(Sys.Date())
 Date <- end.date
@@ -11,11 +9,20 @@ while(difftime(Date, start.date) > 0){
     Date <- as.POSIXlt(as.Date(Date) - 1)
 }
 
-fluorescence.dat <- read.csv("oco-download/fluorescence.csv")
-png("figures/oco.current.png", width=5, height=5, units="in", res=300)
-par(mfrow=c(2,1))
-with(fluorescence.dat,
-	plot(measurement.time, fluorescence.757, type='l')
-	plot(measurement.time, fluorescence.771, type='l')
-)
+
+fluorescence.dat <- read.csv("oco-download/fluorescence.csv", stringsAsFactors=FALSE)
+convert.to.date <- function(x){
+    if(!is.numeric(x)){
+        x <- as.POSIXlt(x)
+    }
+    return(x)
+}
+fluorescence.dat <- as.data.frame(lapply(fluorescence.dat, convert.to.date))
+
+png("figures/oco.current.png", width=5, height=5, units="in", res=150)
+par(mfrow=c(2,1), mar=c(5,5,1,1), cex=1)
+with(fluorescence.dat, {
+	plot(measurement.date, fluorescence.757, type='l')
+	plot(measurement.date, fluorescence.771, type='l')
+})
 dev.off()
